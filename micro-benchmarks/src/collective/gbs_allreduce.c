0590b648996e5b63d19462a843fbf32bc2414b14
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2023-08-30 10:00:58 +0200

    moved output to rank 0

--- micro-benchmarks/src/collective/gbs_allreduce.c
+++ micro-benchmarks/src/collective/gbs_allreduce.c
@@ -45,10 +45,12 @@ int main(int argc, char* argv[]) {
 
 	GASPI_CHECK(gaspi_allreduce_elem_max(&max_elem));
 	if (options.max_message_size > max_elem) {
-		fprintf(stdout,
-		        "limit for allreduce is %d requested was %d\n",
-		        max_elem,
-		        options.max_message_size);
+		if (my_id == 0) {
+			fprintf(stdout,
+			        "limit for allreduce is %d requested was %d\n",
+			        max_elem,
+			        options.max_message_size);
+		}
 		options.max_message_size = max_elem;
 	}
 
