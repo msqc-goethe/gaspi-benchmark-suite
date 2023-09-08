cf210fea533c7e93942c11e794995c4a24e7b43b
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2023-09-08 17:35:11 +0200

    added function pointer based timer to enable runtime change of timing routines

--- micro-benchmarks/util/stopwatch.c
+++ micro-benchmarks/util/stopwatch.c
@@ -1,26 +1,61 @@
-/**
-* @file stopwatch.c
-* @brief
-* @detail 
-* @author Florian Beenen
-* @version 2018-10-26
-*/
-
 #include "stopwatch.h"
 #include <stdlib.h>
 #include <time.h>
+#include "check.h"
+#include "util.h"
+#include "GASPI.h"
+#include "GASPI_Ext.h"
 
+Timer benchmark_timer;
+gaspi_float cpu_freq;
+double cpu_div;
+
+#define stopwatch_secsToNsecs(secs) ((secs) * (uint64_t) 1e9)
+#define stopwatch_secsToNsecs(secs) ((secs) * (uint64_t) 1e9)
+
+stopwatch_t gbs_gettime(void) {
+	struct timespec time;
+	clock_gettime(CLOCK_MONOTONIC, &time);
+	return (stopwatch_t) (stopwatch_secsToNsecs(time.tv_sec) + time.tv_nsec);
+}
+
+// produces really weird results.
+stopwatch_t gaspi_gettime(void) {
+	gaspi_time_t wtime;
+	GASPI_CHECK(gaspi_time_get(&wtime));
+	return (stopwatch_t) (wtime * 1e6); // msecs to nsecs
+}
+
+stopwatch_t gaspi_ticks_time(void) {
+	gaspi_cycles_t ticks;
+	GASPI_CHECK(gaspi_time_ticks(&ticks));
+	return (stopwatch_t)stopwatch_secsToNsecs(ticks * cpu_div);
+}
+
+void init_timer(Timer* t) {
+	switch(options.gaspi_timer){
+		case 0:
+			*t = gbs_gettime;
+			break;
+		case 1:
+			*t = gaspi_gettime;
+			break;
+		case 2:
+			*t = gaspi_ticks_time;
+			break;
+		default:
+			*t = gbs_gettime;
+			break;
+	}
+	GASPI_CHECK(gaspi_cpu_frequency(&cpu_freq));
+	cpu_div = 1 / cpu_freq / (1000.0 * 1000.0);
+}
 
 stopwatch_t stopwatch_start() {
-    struct timespec time;
-    clock_gettime(CLOCK_MONOTONIC, &time);
-    return stopwatch_secsToNsecs(time.tv_sec) + time.tv_nsec;
+	return benchmark_timer();
 }
 
 stopwatch_t stopwatch_stop(stopwatch_t startTime) {
-    return stopwatch_start() - startTime;
-}
-
-double stopwatch_getUsecs(stopwatch_t time) {
-	return (time / 1000.0);
+	stopwatch_t wtime = stopwatch_start() - startTime;
+	return wtime;
 }
